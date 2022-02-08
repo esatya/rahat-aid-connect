24036d253ffa51e383844045d6a1a678d363f590
Manjik Shrestha <myanz.470shrestha@gmail.com>
2022-02-08 11:24:13 +0545

    persist indexed db test

--- src/contexts/AppContext.js
+++ src/contexts/AppContext.js
@@ -14,8 +14,36 @@ export const AppContext = createContext(initialState);
 export const AppContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(appReduce, initialState);
 
+    const isStoragePersisted = async () => {
+  return await navigator.storage && navigator.storage.persisted &&
+    navigator.storage.persisted();
+}
+
+  const persist = async () => {
+    try{
+  return await navigator.storage && navigator.storage.persist &&
+    navigator.storage.persist();
+    }
+    catch(e){
+      console.log(e);
+    }
+}
+
   const checkAidConnectId = useCallback(async _aidConnectId => {
     const { data } = await Service.checkAidConnectId(_aidConnectId);
+    const isPersisted = await isStoragePersisted(); 
+    if (isPersisted) {
+      console.log(":) Storage is successfully persisted.");
+    } else {
+      console.log(":( Storage is not persisted.");
+      console.log("Trying to persist..:");
+      if (await persist()) {
+        console.log(":) We successfully turned the storage to be persisted.");
+      } else {
+        console.log(":( Failed to make storage persisted");
+      }
+    }
+
     dispatch({
       type: APP_ACTIONS.SET_AID_CONNECT_ID,
       data: {
@@ -31,6 +59,8 @@ export const AppContextProvider = ({ children }) => {
     const data = await Service.sendBeneficiaries(state.aidConnectId, beneficiaries);
     return data;
   };
+
+
 
   return (
     <AppContext.Provider
